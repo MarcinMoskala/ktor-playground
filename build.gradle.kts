14aca5354a5d011a397125b75ba74e46f12a9cf6
marcinmoskala <marcinmoskala@gmail.com>
2023-03-09 13:17:56 +0100

    Init

---
+++ build.gradle.kts
@@ -0,0 +1,40 @@
+val ktor_version: String by project
+val kotlin_version: String by project
+val logback_version: String by project
+val ktor_rsocket_version: String by project
+
+plugins {
+    kotlin("jvm") version "1.8.10"
+    id("io.ktor.plugin") version "2.2.4"
+}
+
+group = "com.example"
+version = "0.0.1"
+
+repositories {
+    mavenCentral()
+    maven {
+        url = uri("https://kotlin.bintray.com/ktor")
+    }
+}
+
+application {
+    mainClass.set("com.example.ApplicationKt")
+
+    val isDevelopment: Boolean = project.ext.has("development")
+    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("io.ktor:ktor-server-core-jvm:$ktor_version")
+    implementation("io.ktor:ktor-server-netty-jvm:$ktor_version")
+    implementation("ch.qos.logback:logback-classic:$logback_version")
+    implementation("io.rsocket.kotlin:rsocket-core:$ktor_rsocket_version")
+    implementation("io.rsocket.kotlin:rsocket-ktor-server:$ktor_rsocket_version")
+    testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+}
---
+++ src/main/kotlin/com/example/Application.kt
@@ -0,0 +1,33 @@
+package com.example
+
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+
+fun main() {
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
+        .start(wait = true)
+}
+
+fun Application.module() {
+    routing {
+        get("/") {
+            call.respondText("Hello World!")
+        }
+
+        get("set_cookie") {
+            call.apply {
+                response.cookies.append(Cookie("guid", "SOME_TOKEN"))
+            }.respond(HttpStatusCode.OK)
+        }
+
+        get("respond_cookie") {
+            val cookie: String? = call.request.cookies["guid"]
+            println("Sent cookie: $cookie")
+            call.respond(cookie.orEmpty())
+        }
+    }
+}
