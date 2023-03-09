7c217c68de630cfa355ff9971423dcb5d7fe14ee
marcinmoskala <marcinmoskala@gmail.com>
2023-03-09 13:28:00 +0100

    Add Procfile

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -8,7 +8,7 @@ import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun main() {
-    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
+    embeddedServer(Netty, port = System.getenv("PORT").toInt(), host = "0.0.0.0", module = Application::module)
         .start(wait = true)
 }
 
